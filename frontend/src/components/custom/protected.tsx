6482b77214bbb595fb71dcaafdbdca1377f8f365
WaithakaGuru <waithakaoffices@gmail.com>
2025-09-19 05:38:18 +0300

    Modify the logged in state 'isAuthenticated' to store it in the localstorage for consistency through out the pages

--- frontend/src/components/custom/protected.tsx
+++ frontend/src/components/custom/protected.tsx
@@ -1,11 +1,13 @@
 import { useAuth } from "@/context/authcontext"
-import type {ReactElement} from "react"
+import {useEffect, type ReactElement} from "react"
 import { useNavigate } from "react-router-dom"
 
 function Protected({children} : {children: ReactElement }) {
     const {isAuthenticated} = useAuth()
     const navigate = useNavigate();
-    if(!isAuthenticated) navigate("/")
+    useEffect(()=>{
+        if(!isAuthenticated) navigate("/")
+    }, [isAuthenticated])
     return (
         <>{children}</>
     )
