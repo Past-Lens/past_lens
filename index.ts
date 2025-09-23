063f54b8818772bf9a80390c44d76e1b4f61d461
WaithakaGuru <waithakaoffices@gmail.com>
2025-09-23 09:20:50 +0300

    fix the concurrently frontend command

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import concurrently from "concurrently";
+import concurrently from 'concurrently';
 
 concurrently([
     {
-        name: "Backend",
-        command: "bun index.ts",
-        cwd: "backend",
-        prefixColor: "cyan"
+        name: 'Backend',
+        command: 'bun index.ts',
+        cwd: 'backend',
+        prefixColor: 'cyan',
     },
     {
-        name: "Frontend",
-        command: "bun index.ts",
-        cwd: "frontend",
-        prefixColor: "green"
-    }
-])
+        name: 'Frontend',
+        command: 'bun app.tsx',
+        cwd: 'frontend',
+        prefixColor: 'green',
+    },
+]);
