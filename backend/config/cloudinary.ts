b6033dde1b111f0fe0b4fc1ee417081f896a0244
WaithakaGuru <waithakaoffices@gmail.com>
2025-10-02 11:36:18 +0300

    Remove the configDotenv() method in cloudinary as the variables are automatically loaded by Bun

--- backend/config/cloudinary.ts
+++ backend/config/cloudinary.ts
@@ -1,7 +1,4 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { config as configDotenv } from 'dotenv';
-
-configDotenv();
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
