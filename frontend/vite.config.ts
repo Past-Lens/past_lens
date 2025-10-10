78e8b9243125d89566fa8e17c9257beccdaba4d3
WaithakaGuru <waithakaoffices@gmail.com>
2025-10-10 10:55:41 +0300

    more fixes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,10 +10,4 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    server: {
-        host: true,
-        proxy: {
-            '/api': 'http://localhost:5000',
-        },
-    },
 });
