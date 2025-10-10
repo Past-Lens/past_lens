8b9d87f9c5bf14f3374c6d3c42021e3bd8a66eb2
WaithakaGuru <waithakaoffices@gmail.com>
2025-10-10 15:32:15 +0300

    update remote urls

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -8,16 +8,10 @@ const connectDB = async (): Promise<void> => {
             );
         }
 
-        const conn = await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        } as ConnectOptions);
+        const conn = await mongoose.connect(process.env.DATABASE_URL);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
-        //consolr to show the database name
         console.log(`Database name: ${conn.connection.name}`);
-        //console to show DATABASE_URL
-        // console.log(`DATABASE_URL: ${process.env.DATABASE_URL}`);
     } catch (error: any) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
