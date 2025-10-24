c6dcd0b3df02b75647b51c80d139b37ecede8042
Joy Mbugua <joymbugua007@gmail.com>
2025-10-24 09:31:37 +0300

    fix remote-Backend base axios url

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import useUserStore from '@/stores/userStore';
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:5000/api'
-const baseURL = 'https://past-lensbackend.onrender.com';
+const baseURL = 'https://past-lensbackend.onrender.com/api';
 
 const axInstance = axios.create({
     baseURL: baseURL,
