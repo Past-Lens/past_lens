89b8e3a205a7095ce189643613577f72cea090ef
WaithakaGuru <waithakaoffices@gmail.com>
2025-11-13 23:31:11 +0300

    Removed all the unused 3d folders and dependencies

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -18,7 +18,6 @@ import Stories from './pages/museum/halls/stories';
 import Library from './pages/museum/halls/library';
 import Guide from './pages/museum/guide';
 import Discover from './pages/museum/halls/discover';
-import ImmersiveMuseum from './pages/museum/halls/immersive';
 
 const AppRouter = () => {
     return (
@@ -35,7 +34,6 @@ const AppRouter = () => {
                 <Route path="/museum/halls/stories" element={<Stories />} />
                 <Route path="/museum/halls/library" element={<Library />} />
                 <Route path="/museum/halls/discover" element={<Discover />} />
-                <Route path="/museum/halls/vr" element={<ImmersiveMuseum />} />
                 <Route path="/museum/guide" element={<Guide />} />
                 <Route
                     path="/admin"
